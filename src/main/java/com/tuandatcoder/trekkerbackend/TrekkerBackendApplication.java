a0162e4b3920c508b46f9fa25cc9444670d33db7
TuanDatCoder <tuandatdq03@gmail.com>
2025-11-30 21:44:16 +0700

    first commit

---
+++ src/main/java/com/tuandatcoder/trekkerbackend/TrekkerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tuandatcoder.trekkerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekkerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrekkerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuandatcoder/trekkerbackend/TrekkerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuandatcoder.trekkerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrekkerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
